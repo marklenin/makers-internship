28e43bd18036754358218f7443b92a1700378765
m3marlen <marlenarzymatov87924@gmail.com>
2023-08-04 09:43:11 +0300

    register doing

--- src/components/store/actions/authActions.js
+++ src/components/store/actions/authActions.js
@@ -4,16 +4,26 @@ import { API } from "../../../consts";
 
 export const registerUser = createAsyncThunk(
   "auth/register",
-  async (data, { rejectWithValue }) => {
-    try {
-      let formData = new FormData();
-      formData.append("email", data.Email);
-      formData.append("password", data.Password);
-      formData.append("password_confirm", data.Password_confirm);
-      formData.append("phone", data.Phone);
-      console.log(formData);
+  async (formData, { rejectWithValue }) => {
+    // try {
+    //   let formData = new FormData();
+    //   formData.append("email", data.Email);
+    //   formData.append("password", data.Password);
+    //   formData.append("password_confirm", data.Password_confirm);
+    //   formData.append("phone", data.Phone);
+    //   console.log(formData);
 
-      await axios.post(`${API}account/register/`, formData);
+    //   await axios.post(`${API}account/register/`, formData);
+    // } catch (error) {
+    //   if (error.response && error.response.data.message) {
+    //     return rejectWithValue(error.response.data.message);
+    //   } else {
+    //     return rejectWithValue(error.message);
+    //   }
+    // }
+
+    try {
+      await axios.post(`${API}/account/register/`, formData);
     } catch (error) {
       if (error.response && error.response.data.message) {
         return rejectWithValue(error.response.data.message);
